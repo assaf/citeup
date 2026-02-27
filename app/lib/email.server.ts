041f46b38dbc70c3666224d00981c9d1c51fb740
Assaf Arkin <assaf@labnotes.org>
2026-02-26 16:50:33 -0800

    feat: add Resend email helper for password recovery

---
+++ app/lib/email.server.ts
@@ -0,0 +1,15 @@
+import { Resend } from "resend";
+import envVars from "~/lib/envVars";
+
+const resend = new Resend(envVars.RESEND_API_KEY);
+
+export async function sendPasswordRecoveryEmail(to: string, token: string) {
+  const url = `${envVars.APP_URL}/reset-password/${token}`;
+
+  await resend.emails.send({
+    from: envVars.EMAIL_FROM,
+    to,
+    subject: "Reset your citeup password",
+    html: `<p>Click <a href="${url}">this link</a> to sign in to citeup. The link expires in 30 minutes and can only be used once.</p><p>If you didn't request this, ignore this email.</p>`,
+  });
+}
